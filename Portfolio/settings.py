b89b3bf40f63188160061339572f420f4a74f2f1
Bandar Alrooqi <officialAlrooqi@gmail.com>
2022-12-18 01:48:58 +0300

    push

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -74,11 +74,8 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'portfolio',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'Hgujdfd511'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
